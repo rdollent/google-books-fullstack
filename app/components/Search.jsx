cd68913721be34d3c5916b72d64d29a24c9b59b2
Rod <rumbalumba89@gmail.com>
2019-03-08 18:49:27 +0000

    repeat queries now function properly. if a query already exists in localStorage.list, we take out that entry from the index and place it to the last, seemingly as if it's the latest query.

--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -59,17 +59,20 @@ class Search extends React.Component {
         // https://stackoverflow.com/questions/5410745/how-can-i-get-a-list-of-the-items-stored-in-html-5-local-storage-from-javascript
         // https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
         const initList = JSON.parse(window.localStorage.getItem('list')) || [];
+        
+        // Array.prototype.includes is case-sensitive
+        // if result is already there, just switch its current index to the last one available
+        if(initList.includes(searchWord.toLowerCase())) {
+            // initList.splice
+            initList.splice(initList.indexOf(searchWord.toLowerCase()),1);
+        }
 
         if(initList.length === 10) {
             initList.shift();
         }
 
 
-        // Array.prototype.includes is case-sensitive
-        // if result is already there, just switch its current index to the last one available
-        if(initList.includes(searchWord.toLowerCase())) {
-            // initList.splice
-        }
+        
 
 
         initList.push(searchWord.toLowerCase());
